ba3b4c677d96d4857290c3e9b523c604b88947e7
ryrysan <leeryanjay@gmail.com>
2023-08-31 17:57:43 +0100

    complete the math app

--- ask_help.js
+++ ask_help.js
@@ -1,11 +1,19 @@
+
+
 function toggleHelpContent() {
-  const helpContent = document.getElementById('help-content');
+  // const helpContent = document.getElementById('help-content');
   window.open('ask_help.html', '_blank');
-  
+  // Check if the final result has been triggered
+  if (questionsAnswered === currentQuestions.length) {
+    // Hide the "Need Help?" button
+    document.getElementById('help-button').style.display = 'none';
   }
+  
+}
 
 function submitHelpQuestion() {
   const helpQuestion = document.getElementById('help-question').value;
+  console.log('Submitted question:', helpQuestion);
 
   // You can implement the logic here to handle the submitted question, such as sending it to a server or displaying it on the page.
   // For this example, let's display an alert with the submitted question.
--- chatgpt.js
+++ chatgpt.js
@@ -1,17 +1,20 @@
 const OpenAI = require("openai");
 require('dotenv').config({path:'/Users/ryanlee/dev/ryrysan.github.io/process.env'})
 
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openFun = async() => {
 const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo"
-    messages: [{"role": "user", "content": "can you generate 5 random beginner level addition math questions"}],
-    max_tokens:200
-  });
+  model: "gpt-3.5-turbo",
+  messages: [{"role": "user", "content": "can you generate 5 random beginner level addition math questions"}],
+  max_tokens:200
+});
 
   console.log(chatCompletion.choices[0].message);
 }
 
 openFun();
+
